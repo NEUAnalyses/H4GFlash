e0494f490b1145dfe54f1e90db6ce89e447b2e2b
amassiro <massironi.andrea@gmail.com>
2017-10-23 15:43:57 +0200

    datacard creator update

--- Datacard/cuts.py
+++ Datacard/cuts.py
@@ -1,4 +1,16 @@
 
+
+
 cuts['4gamma'] = 'tp_mass > 100 && tp_mass < 180 && dp1_mass/tp_mass < 0.55   \
                && dp2_mass/tp_mass < 0.50 \
                '
+
+
+
+cuts['4gamma-supercut'] = 'tp_mass > 100 && tp_mass < 180 && dp1_mass/tp_mass < 0.55   \
+               && dp2_mass/tp_mass < 0.50 \
+               && dp2_mass/tp_mass > 0.01 \
+               '
+
+
+
