0ad2b6f7e0de2e7c0a6cd8f1487a5a3ccdebb240
amassiro <massironi.andrea@gmail.com>
2017-10-23 15:38:02 +0200

    datacard creator update

---
+++ Datacard/samples.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+samples['data']  = {  
+
+            'name': [
+              'file1.root',
+              'file2.root',
+              ],
+            
+            'weight' : '1',  
+            
+            'weights' :  [
+                       '1', 
+                       '1.26645',
+                        ],
+           }
+  
+  
+  
+  
+
