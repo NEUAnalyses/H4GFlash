001fb9ba2da316f93bcc9e71793eeaa40e030b71
amassiro <massironi.andrea@gmail.com>
2017-10-23 11:03:02 +0200

    testing mkdatacards

--- Datacard/mkDatacards.py
+++ Datacard/mkDatacards.py
@@ -2,10 +2,27 @@
 ## code to produce a datacard
 ## 
 
+import os
 import os.path
-
-
+import optparse
 from ROOT import *
+
+
+
+
+usage = 'usage: %prog [options]'
+parser = optparse.OptionParser(usage)
+
+parser.add_option('--tag'                , dest='tag'               , help='Tag used for the shape file name'           , default=None)
+parser.add_option('--sigset'             , dest='sigset'            , help='Signal samples [SM]'                        , default='SM')
+parser.add_option('--outputDirDatacard'  , dest='outputDirDatacard' , help='output directory'                           , default='./')
+parser.add_option('--inputFile'          , dest='inputFile'         , help='input directory'                            , default='./input.root')
+parser.add_option('--structureFile'      , dest='structureFile'     , help='file with datacard configurations'          , default=None )
+parser.add_option('--nuisancesFile'      , dest='nuisancesFile'     , help='file with nuisances configurations'         , default=None )
+parser.add_option('--samplesFile'        , dest='samplesFile'       , help='file with samples configurations'         , default=None )
+      
+(opt, args) = parser.parse_args()
+
 
 
 cuts = {}
@@ -18,6 +35,246 @@ for cutName in cuts :
   print "cut = ", cutName, " :: ", cuts[cutName]
 
 
+# ~~~~
+nuisances = {}
+if opt.nuisancesFile == None :
+   print " Please provide the nuisances structure if you want to add nuisances " 
+elif os.path.exists(opt.nuisancesFile) :
+  handle = open(opt.nuisancesFile,'r')
+  exec(handle)
+  handle.close()
+
+# ~~~~
+samples = {}
+if opt.samplesFile == None :
+   print " Please provide the samples file ... sereiously no samples?? " 
+elif os.path.exists(opt.samplesFile) :
+  handle = open(opt.samplesFile,'r')
+  exec(handle)
+  handle.close()
+
+
+#
+# After I got all configurations ...
+#
+
+#
+# ... now let's run ...
+#
+
+outputDirDatacard = 'test/'
+if not os.path.isdir (outputDirDatacard + "/") :
+  os.mkdir(outputDirDatacard + "/")
+
+
+#---- loop over "cuts"
+for cutName in cuts :
+  
+  signals     = ['sig']
+  backgrounds = ['bkg']
+  
+  # calculate yields for data, signal and background
+  yieldsSig  = {}
+  yieldsBkg  = {}
+  yieldsData = {}
+
+  #
+  # to do it properly :-)
+  #
+  yieldsData['data'] = 1.0
+  yieldsBkg['bkg'] = 10.3
+  yieldsSig['sig'] = 100.999
+  
+  
+  
+  
+  if not os.path.isdir (outputDirDatacard + "/" + cutName) :
+    os.mkdir(outputDirDatacard + "/" + cutName)
+  
+  tagNameToAppearInDatacard = cutName
+  
+  
+  cardPath = outputDirDatacard + "/" + cutName + "/datacard.txt"
+  print 'Writing to ' + cardPath 
+  card = open( cardPath ,"w")
+  card.write('## Parametric shape input card\n')
+  
+  card.write('imax 1 number of channels\n')
+  card.write('jmax * number of background\n')
+  card.write('kmax * number of nuisance parameters\n') 
+  
+  card.write('-'*100+'\n')
+  card.write('bin         %s' % tagNameToAppearInDatacard+'\n')
+  
+  card.write('observation %.0f\n' % yieldsData['data'])
+  
+  card.write('shapes  *           * '+
+             'shapes/histos_' + tagNameToAppearInDatacard + ".root" +
+             '     histo_$PROCESS histo_$PROCESS_$SYSTEMATIC' + '\n')
+  
+  card.write('shapes  data_obs           * '+
+             'shapes/histos_' + tagNameToAppearInDatacard + ".root" +
+             '     histo_Data' + '\n')
+  
+  #   shapes  *           * shapes/hww-19.36fb.mH125.of_vh2j_shape_mll.root     histo_$PROCESS histo_$PROCESS_$SYSTEMATIC
+  #   shapes  data_obs    * shapes/hww-19.36fb.mH125.of_vh2j_shape_mll.root     histo_Data
+  
+  
+  totalNumberSamples = len(yieldsSig) + len(yieldsBkg)
+  columndef = 30
+  
+  # adapt column length to long bin names            
+  if len(tagNameToAppearInDatacard) >= (columndef - 5) :
+    columndef = len(tagNameToAppearInDatacard) + 7
+  
+  
+  card.write('bin'.ljust(80) + ''.join( [tagNameToAppearInDatacard.ljust(columndef) * totalNumberSamples])+'\n')
+  
+  card.write('process'.ljust(80))
+  card.write(''.join([name.ljust(columndef) for name in signals]))
+  card.write(''.join([name.ljust(columndef) for name in backgrounds]))
+  card.write('\n')
+  
+  card.write('process'.ljust(80))
+  card.write(''.join([('%d' % -iSample   ).ljust(columndef) for iSample in range(len(signals))     ]))
+  card.write(''.join([('%d' % (iSample+1)).ljust(columndef) for iSample in range(len(backgrounds)) ]))
+  card.write('\n')
+  
+  card.write('rate'.ljust(80))
+  card.write(''.join([('%-.4f' % yieldsSig[name]).ljust(columndef) for name in signals    ]))
+  card.write(''.join([('%-.4f' % yieldsBkg[name]).ljust(columndef) for name in backgrounds]))
+  card.write('\n')
+  
+  card.write('-'*100+'\n')
+  
+  # add nuisances
+  
+  # first the lnN nuisances
+  for nuisanceName, nuisance in nuisances.iteritems():
+    
+    print "nuisanceName"
+    
+    ## check if a nuisance can be skipped because not in this particular cut
+    #use_this_nuisance = False
+    #if  'cuts' in nuisance.keys() :
+      #for Cuts_where_to_use_nuisance  in   nuisance['cuts'] :
+        #if Cuts_where_to_use_nuisance == cutName :
+          ## use this niusance
+          #use_this_nuisance = True
+    #else :
+      ## default is use the nuisance everywhere
+      #use_this_nuisance = True 
+    
+    #if use_this_nuisance :
+     
+      #if nuisanceName != 'stat' : # 'stat' has a separate treatment, it's the MC/data statistics
+        
+        #if 'type' in nuisance.keys() : # some nuisances may not have "type" ... why?
+          ##print "nuisance[type] = ", nuisance ['type']
+          #if nuisance ['type'] == 'lnN' or nuisance ['type'] == 'lnU' :
+            #card.write((nuisance['name']).ljust(80-20))
+            #card.write((nuisance ['type']).ljust(20))
+            #if 'all' in nuisance.keys() and nuisance ['all'] == 1 : # for all samples
+              ##card.write(''.join([('%-.4f' % nuisance['value']).ljust(columndef) for name in signals      ]))
+              ##card.write(''.join([('%-.4f' % nuisance['value']).ljust(columndef) for name in backgrounds  ]))
+              #card.write(''.join([(' %s ' % nuisance['value']).ljust(columndef) for name in signals      ]))
+              #card.write(''.join([(' %s ' % nuisance['value']).ljust(columndef) for name in backgrounds  ]))
+              #card.write('\n')
+            #else :
+              ## apply only to selected samples
+              #for sampleName in signals:
+                #if sampleName in nuisance['samples'].keys() :
+                  ##card.write(('%-.4f' % nuisance['samples'][sampleName]).ljust(columndef))
+                  #card.write(('%s' % nuisance['samples'][sampleName]).ljust(columndef))
+                #else :
+                  #card.write(('-').ljust(columndef))
+              #for sampleName in backgrounds:
+                #if sampleName in nuisance['samples'].keys() :
+                  ##card.write(('%-.4f' % nuisance['samples'][sampleName]).ljust(columndef))
+                  #card.write(('%s' % nuisance['samples'][sampleName]).ljust(columndef))
+                #else :
+                  #card.write(('-').ljust(columndef))
+                   
+          #elif nuisance ['type'] == 'shape' :
+            #card.write(("CMS_" + (nuisance['name'])).ljust(80-20))
+            #if 'AsLnN' in nuisance.keys() and  float(nuisance ['AsLnN']) >= 1:
+              #print ">>>>>", nuisance['name'], " was derived as a shape uncertainty but is being treated as a lnN"
+              #card.write(('lnN').ljust(20))
+              #allSelectedSamples = signals + backgrounds  
+              #for sampleName in allSelectedSamples:
+                #if ('all' in nuisance.keys() and nuisance ['all'] == 1) or \
+                   #sampleName in nuisance['samples'].keys() :  
+                  #histo     = _getHisto(cutName+"/"+variableName+'/', 
+                                             #'histo_' + sampleName)
+                  #histoUp   = _getHisto(cutName+"/"+variableName+'/', 
+                                             #'histo_' + sampleName + '_' + (nuisance['name']) + "Up") 
+                  #histoDown = _getHisto(cutName+"/"+variableName+'/',
+                                             #'histo_' + sampleName + '_' + (nuisance['name']) + "Down")
+  
+                  #histoIntegral = histo.Integral()
+                  #histoUpIntegral = histoUp.Integral()
+                  #histoDownIntegral = histoDown.Integral()
+                  #if histoIntegral > 0:
+                    #diffUp = (histoUpIntegral - histoIntegral)/histoIntegral/float(nuisance ['AsLnN'])
+                    #diffDo = (histoDownIntegral - histoIntegral)/histoIntegral/float(nuisance ['AsLnN'])
+                  #else: 
+                    #diffUp = 0.
+                    #diffDo = 0.
+  
+                  #lnNUp = 1. + diffUp
+                  #lnNDo = 1. + diffDo
+                    
+                  #card.write((('%-.4f' % lnNUp)+"/"+('%-.4f' % lnNDo)).ljust(columndef))
+                #else:
+                  #card.write(('-').ljust(columndef)) 
+  
+            #else:  
+              #card.write((nuisance ['type']).ljust(20))
+              #if 'all' in nuisance.keys() and nuisance ['all'] == 1 : # for all samples
+                #card.write(''.join([('1.000').ljust(columndef) for name in signals      ]))
+                #card.write(''.join([('1.000').ljust(columndef) for name in backgrounds  ]))
+                #card.write('\n')
+              #else :
+                ## apply only to selected samples
+                #for sampleName in signals:
+                  #if sampleName in nuisance['samples'].keys() :
+                    #card.write(('1.000').ljust(columndef))                          
+                    ## save the nuisance histograms in the root file
+                    #_saveHisto(cutName+"/"+variableName+'/',
+                                     #'histo_' + sampleName + '_' + (nuisance['name']) + "Up",
+                                     #'histo_' + sampleName + '_CMS_' + (nuisance['name']) + "Up"
+                                     #)
+                    #_saveHisto(cutName+"/"+variableName+'/',
+                                     #'histo_' + sampleName + '_' + (nuisance['name']) + "Down",
+                                     #'histo_' + sampleName + '_CMS_' + (nuisance['name']) + "Down"
+                                     #)
+                  #else :
+                    #card.write(('-').ljust(columndef))
+                #for sampleName in backgrounds:
+                  #if sampleName in nuisance['samples'].keys() :
+                    #card.write(('1.000').ljust(columndef))
+                    ## save the nuisance histograms in the root file
+                    #_saveHisto(cutName+"/"+variableName+'/',
+                                     #'histo_' + sampleName + '_' + (nuisance['name']) + "Up",
+                                     #'histo_' + sampleName + '_CMS_' + (nuisance['name']) + "Up"
+                                     #)
+                    #_saveHisto(cutName+"/"+variableName+'/',
+                                     #'histo_' + sampleName + '_' + (nuisance['name']) + "Down",
+                                     #'histo_' + sampleName + '_CMS_' + (nuisance['name']) + "Down"
+                                     #)
+                  #else :
+                    #card.write(('-').ljust(columndef))
+            
+          ## new line at the end of any nuisance that is *not* stat ... because in that case it's already done on its own
+        #card.write('\n')
+                 
+  card.write('-'*100+'\n')
+
+  card.write('\n')
+  card.close()
+
+
+  
 ##first loop over all variables
 #for v in Vars:
    #hists = []
