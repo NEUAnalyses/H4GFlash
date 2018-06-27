6ab64372048bb5a492c9967875f5250c43cebe8d
wamorkart <wamorkar.t@husky.neu.edu>
2018-06-27 13:02:02 +0200

    make trees for gen level study

--- AnalyzerPy/AnalyzerTree.py
+++ AnalyzerPy/AnalyzerTree.py
@@ -7,8 +7,7 @@ from array import array
 from analyzertools import *
 
 def main(argv):
-   #inputfiles = '/eos/cms/store/user/torimoto/physics/4gamma/2018/Signal/sig50.root'
-   inputfiles = '/eos/user/t/twamorka/FatPho0p1_Match0p15/sig1.root'
+   inputfiles = '/eos/cms/store/user/twamorka/Signal_HADD/signal_1.root'
    outputfile = 'test.root'
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["inputFiles=","outputFile="])
@@ -37,9 +36,9 @@ def main(argv):
 
    Genmaker = GenTools()
    outTree = Genmaker.MakeGenTree()
-   outTree_case1 = Genmaker.MakeGenTree_case1()
-   outTree_case2 = Genmaker.MakeGenTree_case2()
-   outTree_case3 = Genmaker.MakeGenTree_case3()
+   outTree_4resolved = Genmaker.MakeGenTree_4resolved()
+   outTree_2res1fat = Genmaker.MakeGenTree_2res1fat()
+   outTree_2fat = Genmaker.MakeGenTree_2fat()
 
    triggers = {}
    triggerNames = []
@@ -55,30 +54,30 @@ def main(argv):
       tree.GetEntry(evt)
       Genphos = []
       number = 0
-      for g in range(0,tree.v_photonDaughters_p4.size()):       ## loop over GEN photons
+      for g in range(0,tree.v_genlevelphoton_p4.size()):       ## loop over GEN photons
          P4 = TLorentzVector(0,0,0,0)
-         P4.SetPtEtaPhiE( tree.v_photonDaughters_p4[g].pt(), tree.v_photonDaughters_p4[g].eta(), tree.v_photonDaughters_p4[g].phi(), tree.v_photonDaughters_p4[g].e())
-         
+         P4.SetPtEtaPhiE( tree.v_genlevelphoton_p4[g].pt(), tree.v_genlevelphoton_p4[g].eta(), tree.v_genlevelphoton_p4[g].phi(), tree.v_genlevelphoton_p4[g].e())
+
          Genphos.append(P4)
          minDR = 999
-      
+
       Photon1 = TLorentzVector(0,0,0,0)
       Photon2 = TLorentzVector(0,0,0,0)
       Photon3 = TLorentzVector(0,0,0,0)
       Photon4 = TLorentzVector(0,0,0,0)
-      
+
       Photon1 = Genphos[0] # photons 1,2 come from a1; 3,4 come from a2
       Photon2 = Genphos[1]
       Photon3 = Genphos[2]
       Photon4 = Genphos[3]
-      
+
       Genmaker.gen12_mass[0] = (Photon1+Photon2).M()
       Genmaker.gen13_mass[0] = (Photon1+Photon3).M()
       Genmaker.gen14_mass[0] = (Photon1+Photon4).M()
       Genmaker.gen23_mass[0] = (Photon2+Photon3).M()
       Genmaker.gen24_mass[0] = (Photon2+Photon4).M()
       Genmaker.gen34_mass[0] = (Photon3+Photon4).M()
-      
+
       Genmaker.gen12dr[0] = Photon1.DeltaR(Photon2)
       Genmaker.gen13dr[0] = Photon1.DeltaR(Photon3)
       Genmaker.gen14dr[0] = Photon1.DeltaR(Photon4)
@@ -86,16 +85,20 @@ def main(argv):
       Genmaker.gen24dr[0] = Photon2.DeltaR(Photon4)
       Genmaker.gen34dr[0] = Photon3.DeltaR(Photon4)
 
+      Genmaker.gen_mindr[0] = min(Photon1.DeltaR(Photon2),Photon1.DeltaR(Photon3),Photon1.DeltaR(Photon4),Photon2.DeltaR(Photon3),Photon2.DeltaR(Photon4),Photon3.DeltaR(Photon4))
+      Genmaker.gen_maxdr[0] = max(Photon1.DeltaR(Photon2),Photon1.DeltaR(Photon3),Photon1.DeltaR(Photon4),Photon2.DeltaR(Photon3),Photon2.DeltaR(Photon4),Photon3.DeltaR(Photon4))
+
+
       drlist = []
       fatphoton = []
       drlist.append(Photon1.DeltaR(Photon2))
       drlist.append(Photon3.DeltaR(Photon4))
-      
-      
+
+
       Genphos.sort(key=lambda x: x.Pt(), reverse=True) # now sort in pt order and save kinematics
-      
-      
-      
+
+
+
       if len(Genphos) < 4:
          continue
 
@@ -103,8 +106,8 @@ def main(argv):
       gen2 = TLorentzVector(0,0,0,0)
       gen3 = TLorentzVector(0,0,0,0)
       gen4 = TLorentzVector(0,0,0,0)
-      
-      
+
+
       acc = []
       acceptance = ""
       pho1out = []
@@ -126,8 +129,8 @@ def main(argv):
          gen3 = Genphos[2]
       if len(Genphos) > 3:
          gen4 = Genphos[3]
-      
-      
+
+
       gen12 = gen1+gen2
       gen13 = gen1+gen3
       gen14 = gen1+gen4
@@ -135,8 +138,7 @@ def main(argv):
       gen24 = gen2+gen4
       gen34 = gen3+gen4
 
-#print gen1.Pt() , gen2.Pt(), gen3.Pt(), gen4.Pt()
-      
+
       Genmaker.gen1_pt[0] = gen1.Pt()
       Genmaker.gen2_pt[0] = gen2.Pt()
       Genmaker.gen3_pt[0] = gen3.Pt()
@@ -145,16 +147,24 @@ def main(argv):
       Genmaker.gen2_eta[0] = gen2.Eta()
       Genmaker.gen3_eta[0] = gen3.Eta()
       Genmaker.gen4_eta[0] = gen4.Eta()
+      Genmaker.gen1_phi[0] = gen1.Phi()
+      Genmaker.gen2_phi[0] = gen2.Phi()
+      Genmaker.gen3_phi[0] = gen3.Phi()
+      Genmaker.gen4_phi[0] = gen4.Phi()
+      Genmaker.gen1_energy[0] = gen1.Energy()
+      Genmaker.gen2_energy[0] = gen2.Energy()
+      Genmaker.gen3_energy[0] = gen3.Energy()
+      Genmaker.gen4_energy[0] = gen4.Energy()
       Genmaker.tp_mass[0] = (gen1+gen2+gen3+gen4).M()
 
       outTree.Fill()
-   
 
-
-
-      
+#
+#
+#
+#
       for d in drlist:
-          if d < 0.1:
+          if d < 0.15:
              fatphoton.append(1)
 
       category = ""
@@ -166,10 +176,10 @@ def main(argv):
           category = 2            ## 2 merged pairs
       else : print " i give up"
 
-
-       
-    
-
+#
+#
+#
+#
       pho1 = ""
       pho2 = ""
       pho3 = ""
@@ -179,33 +189,16 @@ def main(argv):
       twopho = []
       if category == 4:  # all photons are resolved
 
-         pho1 = gen1
-         pho2 = gen2
-         pho3 = gen3
-         pho4 = gen4
-         
+         pho1 = Photon1
+         pho2 = Photon2
+         pho3 = Photon3
+         pho4 = Photon4
+
          fourpho.append(pho1)
          fourpho.append(pho2)
          fourpho.append(pho3)
          fourpho.append(pho4)
-         #print " 4 photon case"
-         #print pho1.Pt() , pho2.Pt(), pho3.Pt(), pho4.Pt()
-         fourpho.sort(key=lambda x: x.Pt(), reverse=True) # now sort in pt order and save kinematics
-         
-         pho1 = fourpho[0]
-         pho2 = fourpho[1]
-         pho3 = fourpho[2]
-         pho4 = fourpho[3]
-         
-         #print pho1.Pt() , pho2.Pt(), pho3.Pt(), pho4.Pt()
-         Genmaker.p1_pt_case1[0] = pho1.Pt()
-         Genmaker.p2_pt_case1[0] = pho2.Pt()
-         Genmaker.p3_pt_case1[0] = pho3.Pt()
-         Genmaker.p4_pt_case1[0] = pho4.Pt()
-         Genmaker.p1_eta_case1[0] = pho1.Eta()
-         Genmaker.p2_eta_case1[0] = pho2.Eta()
-         Genmaker.p3_eta_case1[0] = pho3.Eta()
-         Genmaker.p4_eta_case1[0] = pho4.Eta()
+
          Genmaker.p12_dr_case1[0] = pho1.DeltaR(pho2)
          Genmaker.p13_dr_case1[0] = pho1.DeltaR(pho3)
          Genmaker.p14_dr_case1[0] = pho1.DeltaR(pho4)
@@ -219,8 +212,26 @@ def main(argv):
          Genmaker.p24_mass_case1[0] = (pho2+pho4).M()
          Genmaker.p34_mass_case1[0] = (pho3+pho4).M()
          Genmaker.tp_mass_case1[0] = (pho1+pho2+pho3+pho4).M()
-         
-         
+
+         fourpho.sort(key=lambda x: x.Pt(), reverse=True) # now sort in pt order and save kinematics
+
+         pho1 = fourpho[0]
+         pho2 = fourpho[1]
+         pho3 = fourpho[2]
+         pho4 = fourpho[3]
+
+         #print pho1.Pt() , pho2.Pt(), pho3.Pt(), pho4.Pt()
+         Genmaker.p1_pt_case1[0] = pho1.Pt()
+         Genmaker.p2_pt_case1[0] = pho2.Pt()
+         Genmaker.p3_pt_case1[0] = pho3.Pt()
+         Genmaker.p4_pt_case1[0] = pho4.Pt()
+         Genmaker.p1_eta_case1[0] = pho1.Eta()
+         Genmaker.p2_eta_case1[0] = pho2.Eta()
+         Genmaker.p3_eta_case1[0] = pho3.Eta()
+         Genmaker.p4_eta_case1[0] = pho4.Eta()
+
+
+
          if (pho1.Pt() < 30 or abs(pho1.Eta()) > 2.5):
             pho1out.append(1)
          if (pho2.Pt() < 18 or abs(pho2.Eta()) > 2.5):
@@ -229,16 +240,16 @@ def main(argv):
             pho3out.append(1)
          if (pho4.Pt() < 10 or abs(pho4.Eta()) > 2.5):
             pho4out.append(1)
-         
+
          Genmaker.Pho1out[0] = len(pho1out)
          Genmaker.Pho2out[0] = len(pho2out)
          Genmaker.Pho3out[0] = len(pho3out)
          Genmaker.Pho4out[0] = len(pho4out)
 
-         outTree_case1.Fill()
+         outTree_4resolved.Fill()
 
       elif category == 3: # 1 fat (sorry, merged :P) photon
-    
+
 
            if  Photon1.DeltaR(Photon2) == min(drlist):
                pho1 = Photon1+Photon2
@@ -259,46 +270,46 @@ def main(argv):
            threepho.append(pho1)
            threepho.append(pho2)
            threepho.append(pho3)
+           Genmaker.fatpho_isopho1_dr_case2[0] = pho1.DeltaR(pho2)
+           Genmaker.fatpho_isopho2_dr_case2[0] = pho1.DeltaR(pho3)
+           Genmaker.isopho1_isopho2_dr_case2[0] = pho2.DeltaR(pho3)
+           Genmaker.fatpho_mass_case2[0] = pho1.M()
+           Genmaker.isopho12_mass_case2[0] = (pho2+pho3).M()
            threepho.sort(key=lambda x: x.Pt(), reverse=True)
            pho1 = threepho[0]
            pho2 = threepho[1]
            pho3 = threepho[2]
-      #print pho1.Pt() , pho2.Pt(), pho3.Pt()
            Genmaker.fatpho_pt_case2[0]  = pho1.Pt()
            Genmaker.isopho1_pt_case2[0] = pho2.Pt()
            Genmaker.isopho2_pt_case2[0] = pho3.Pt()
            Genmaker.fatpho_eta_case2[0] = pho1.Eta()
            Genmaker.isopho1_eta_case2[0] = pho2.Eta()
            Genmaker.isopho2_eta_case2[0] = pho3.Eta()
-           Genmaker.fatpho_isopho1_dr_case2[0] = pho1.DeltaR(pho2)
-           Genmaker.fatpho_isopho2_dr_case2[0] = pho1.DeltaR(pho3)
-           Genmaker.isopho1_isopho2_dr_case2[0] = pho2.DeltaR(pho3)
-           Genmaker.fatpho_mass_case2[0] = pho1.M()
-           Genmaker.isopho12_mass_case2[0] = (pho2+pho3).M()
+
            Genmaker.tp_mass_case2[0] = (pho1+pho2+pho3).M()
-           
+
            if (pho1.Pt() < 30 or abs(pho1.Eta()) > 2.5):
               fatphoout.append(1)
            if (pho2.Pt() < 18 or abs(pho2.Eta()) > 2.5):
               isopho1out.append(1)
            if (pho3.Pt() < 10 or abs(pho3.Eta()) > 2.5):
               isopho2out.append(1)
-           
+
            Genmaker.Fatphoout[0] = len(fatphoout)
            Genmaker.Isopho1out[0] = len(isopho1out)
            Genmaker.Isopho2out[0] = len(isopho2out)
-           
-           outTree_case2.Fill()
- 
 
+           outTree_2res1fat.Fill()
 
-
+#
+#
+#
       elif category == 2:
-          
+
            if Photon1.DeltaR(Photon2) == min(drlist):
               pho1 = Photon1+Photon2
               pho2 = Photon3+Photon4
- 
+
            elif Photon3.DeltaR(Photon4) == min(drlist):
                 pho1 = Photon3+Photon4
                 pho2 = Photon1+Photon2
@@ -307,7 +318,6 @@ def main(argv):
            twopho.sort(key=lambda x: x.Pt(), reverse=True)
            pho1 = twopho[0]
            pho2 = twopho[1]
-           #print pho1.Pt() , pho2.Pt()
            Genmaker.fatpho1_pt_case3[0] = pho1.Pt()
            Genmaker.fatpho2_pt_case3[0] = pho2.Pt()
            Genmaker.fatpho1_eta_case3[0] = pho1.Eta()
@@ -321,11 +331,11 @@ def main(argv):
                fatpho1out.append(1)
            if (pho2.Pt() < 18 or abs(pho1.Eta()) > 2.5):
                fatpho2out.append(1)
-           
+
            Genmaker.Fatpho1out[0] = len(fatpho1out)
            Genmaker.Fatpho2out[0] = len(fatpho2out)
-           
-           outTree_case3.Fill()
+
+           outTree_2fat.Fill()
 
 
 
@@ -334,11 +344,11 @@ def main(argv):
 
    outRoot.cd()
    outTree.Write()
-   outTree_case1.Write()
-   outTree_case2.Write()
-   outTree_case3.Write()
+   outTree_4resolved.Write()
+   outTree_2res1fat.Write()
+   outTree_2fat.Write()
 
-   
+
    outRoot.Close()
 
 
--- AnalyzerPy/analyzertools.py
+++ AnalyzerPy/analyzertools.py
@@ -15,6 +15,14 @@ class GenTools:
        self.gen2_eta = n.zeros(1, dtype=float)
        self.gen3_eta = n.zeros(1, dtype=float)
        self.gen4_eta = n.zeros(1, dtype=float)
+       self.gen1_phi = n.zeros(1, dtype=float)
+       self.gen2_phi = n.zeros(1, dtype=float)
+       self.gen3_phi = n.zeros(1, dtype=float)
+       self.gen4_phi = n.zeros(1, dtype=float)
+       self.gen1_energy = n.zeros(1, dtype=float)
+       self.gen2_energy = n.zeros(1, dtype=float)
+       self.gen3_energy = n.zeros(1, dtype=float)
+       self.gen4_energy = n.zeros(1, dtype=float)
        self.gen12_mass = n.zeros(1, dtype=float)
        self.gen13_mass = n.zeros(1, dtype=float)
        self.gen14_mass = n.zeros(1, dtype=float)
@@ -27,9 +35,11 @@ class GenTools:
        self.gen23dr = n.zeros(1, dtype=float)
        self.gen24dr = n.zeros(1, dtype=float)
        self.gen34dr = n.zeros(1, dtype=float)
+       self.gen_mindr = n.zeros(1, dtype=float)
+       self.gen_maxdr = n.zeros(1, dtype=float)
        self.tp_mass = n.zeros(1, dtype=float)
-       
-       
+
+       #
        self.p1_pt_case1 = n.zeros(1, dtype=float)
        self.p2_pt_case1 = n.zeros(1, dtype=float)
        self.p3_pt_case1 = n.zeros(1, dtype=float)
@@ -38,30 +48,58 @@ class GenTools:
        self.p2_eta_case1 = n.zeros(1, dtype=float)
        self.p3_eta_case1 = n.zeros(1, dtype=float)
        self.p4_eta_case1 = n.zeros(1, dtype=float)
+       self.p12_dr_case1 = n.zeros(1, dtype=float)
+       self.p13_dr_case1 = n.zeros(1, dtype=float)
+       self.p14_dr_case1 = n.zeros(1, dtype=float)
+       self.p23_dr_case1 = n.zeros(1, dtype=float)
+       self.p24_dr_case1 = n.zeros(1, dtype=float)
+       self.p34_dr_case1 = n.zeros(1, dtype=float)
+       self.p12_mass_case1 = n.zeros(1, dtype=float)
+       self.p13_mass_case1 = n.zeros(1, dtype=float)
+       self.p14_mass_case1 = n.zeros(1, dtype=float)
+       self.p23_mass_case1 = n.zeros(1, dtype=float)
+       self.p24_mass_case1 = n.zeros(1, dtype=float)
+       self.p34_mass_case1 = n.zeros(1, dtype=float)
        self.tp_mass_case1 = n.zeros(1, dtype=float)
-       
-       
+       self.Pho1out = n.zeros(1, dtype=float)
+       self.Pho2out = n.zeros(1, dtype=float)
+       self.Pho3out = n.zeros(1, dtype=float)
+       self.Pho4out = n.zeros(1, dtype=float)
+
        self.fatpho_pt_case2 = n.zeros(1, dtype=float)
        self.isopho1_pt_case2 = n.zeros(1, dtype=float)
        self.isopho2_pt_case2 = n.zeros(1, dtype=float)
        self.fatpho_eta_case2 = n.zeros(1, dtype=float)
        self.isopho1_eta_case2 = n.zeros(1, dtype=float)
        self.isopho2_eta_case2 = n.zeros(1, dtype=float)
+       self.fatpho_isopho1_dr_case2 = n.zeros(1, dtype=float)
+       self.fatpho_isopho2_dr_case2 = n.zeros(1, dtype=float)
+       self.isopho1_isopho2_dr_case2 = n.zeros(1, dtype=float)
+       self.fatpho_mass_case2 = n.zeros(1, dtype=float)
+       self.isopho12_mass_case2 = n.zeros(1, dtype=float)
        self.tp_mass_case2 = n.zeros(1, dtype=float)
-       self.fatpho_dr_case3 = n.zeros(1, dtype=float)
-
+       self.Fatphoout = n.zeros(1, dtype=float)
+       self.Isopho1out = n.zeros(1, dtype=float)
+       self.Isopho2out = n.zeros(1, dtype=float)
+       #
+       #
        self.fatpho1_pt_case3 = n.zeros(1, dtype=float)
        self.fatpho1_eta_case3 = n.zeros(1, dtype=float)
        self.fatpho2_pt_case3 = n.zeros(1, dtype=float)
        self.fatpho2_eta_case3 = n.zeros(1, dtype=float)
-       self.fatpho1_dr_case3 = n.zeros(1, dtype=float)
-       self.fatpho2_dr_case3 = n.zeros(1, dtype=float)
+       self.fatpho1_fatpho2_dr_case3 = n.zeros(1, dtype=float)
+       self.fatpho1_mass_case3 = n.zeros(1, dtype=float)
+       self.fatpho2_mass_case3 = n.zeros(1, dtype=float)
        self.tp_mass_case3 = n.zeros(1, dtype=float)
-   
+       self.Fatpho1out = n.zeros(1, dtype=float)
+       self.Fatpho2out = n.zeros(1, dtype=float)
+
+
+
    def MakeGenTree(self):
        outTree = TTree("geninfo","tot evs")
        SetOwnership(outTree,0)
-   
+
        outTree.Branch('totevs',self.totevs,'totevs/D')
        outTree.Branch('gen1_pt',self.gen1_pt,'gen1_pt/D')
        outTree.Branch('gen2_pt',self.gen2_pt,'gen2_pt/D')
@@ -71,6 +109,14 @@ class GenTools:
        outTree.Branch('gen2_eta',self.gen2_eta,'gen2_eta/D')
        outTree.Branch('gen3_eta',self.gen3_eta,'gen3_eta/D')
        outTree.Branch('gen4_eta',self.gen4_eta,'gen4_eta/D')
+       outTree.Branch('gen1_phi',self.gen1_phi,'gen1_phi/D')
+       outTree.Branch('gen2_phi',self.gen2_phi,'gen2_phi/D')
+       outTree.Branch('gen3_phi',self.gen3_phi,'gen3_phi/D')
+       outTree.Branch('gen4_phi',self.gen4_phi,'gen4_phi/D')
+       outTree.Branch('gen1_energy',self.gen1_energy,'gen1_energy/D')
+       outTree.Branch('gen2_energy',self.gen2_energy,'gen2_energy/D')
+       outTree.Branch('gen3_energy',self.gen3_energy,'gen3_energy/D')
+       outTree.Branch('gen4_energy',self.gen4_energy,'gen4_energy/D')
        outTree.Branch('gen12_mass',self.gen12_mass,'gen12_mass/D')
        outTree.Branch('gen13_mass',self.gen13_mass,'gen13_mass/D')
        outTree.Branch('gen14_mass',self.gen14_mass,'gen14_mass/D')
@@ -83,12 +129,14 @@ class GenTools:
        outTree.Branch('gen23dr',self.gen23dr,'gen23dr/D')
        outTree.Branch('gen24dr',self.gen24dr,'gen24dr/D')
        outTree.Branch('gen34dr',self.gen34dr,'gen34dr/D')
+       outTree.Branch('gen_mindr',self.gen_mindr,'gen_mindr/D')
+       outTree.Branch('gen_maxdr',self.gen_maxdr,'gen_maxdr/D')
        outTree.Branch('tp_mass',self.tp_mass,'tp_mass/D')
-       
+
        return outTree
-   
-   def MakeGenTree_case1(self):
-       outTree_case1 = TTree("H4GGen_case1", "4 resolved")
+
+   def MakeGenTree_4resolved(self):
+       outTree_case1 = TTree("H4GGen_4resolved", "4 resolved")
        SetOwnership(outTree_case1,0)
 
        outTree_case1.Branch('p1_pt_case1', self.p1_pt_case1, 'p1_pt_case1/D')
@@ -99,59 +147,64 @@ class GenTools:
        outTree_case1.Branch('p2_eta_case1', self.p2_eta_case1, 'p2_eta_case1/D')
        outTree_case1.Branch('p3_eta_case1', self.p3_eta_case1, 'p3_eta_case1/D')
        outTree_case1.Branch('p4_eta_case1', self.p4_eta_case1, 'p4_eta_case1/D')
+       outTree_case1.Branch('p12_dr_case1', self.p12_dr_case1,  'p12_dr_case1/D')
+       outTree_case1.Branch('p13_dr_case1', self.p13_dr_case1,  'p13_dr_case1/D')
+       outTree_case1.Branch('p14_dr_case1', self.p14_dr_case1,  'p14_dr_case1/D')
+       outTree_case1.Branch('p23_dr_case1', self.p23_dr_case1,  'p23_dr_case1/D')
+       outTree_case1.Branch('p24_dr_case1', self.p24_dr_case1,  'p24_dr_case1/D')
+       outTree_case1.Branch('p34_dr_case1', self.p34_dr_case1,  'p34_dr_case1/D')
+       outTree_case1.Branch('p12_mass_case1', self.p12_mass_case1,  'p12_mass_case1/D')
+       outTree_case1.Branch('p13_mass_case1', self.p13_mass_case1,  'p13_mass_case1/D')
+       outTree_case1.Branch('p14_mass_case1', self.p14_mass_case1,  'p14_mass_case1/D')
+       outTree_case1.Branch('p23_mass_case1', self.p23_mass_case1,  'p23_mass_case1/D')
+       outTree_case1.Branch('p24_mass_case1', self.p24_mass_case1,  'p24_mass_case1/D')
+       outTree_case1.Branch('p34_mass_case1', self.p34_mass_case1,  'p34_mass_case1/D')
        outTree_case1.Branch('tp_mass_case1', self.tp_mass_case1, 'tp_mass_case1/D')
-       #outTree.Branch('a1_dr',self.a1_dr,'a1_dr/D')
-       #outTree.Branch('a2_dr',self.a2_dr,'a2_dr/D')
-       #outTree.Branch('a1_deta',self.a1_deta,'a1_deta/D')
-       #outTree.Branch('a2_deta',self.a2_deta,'a2_deta/D')
-       #outTree.Branch('a1_dphi',self.a1_dphi,'a1_dphi/D')
-       #outTree.Branch('a2_dphi',self.a2_dphi,'a2_dphi/D')
-       #outTree.Branch('a1_mass',self.a1_mass,'a1_mass/D')
-       #outTree.Branch('a1_mass',self.a1_mass,'a1_mass/D')
-       #outTree.Branch('a1_pt',self.a1_pt,'a1_pt/D')
-       #outTree.Branch('a2_pt',self.a2_pt,'a2_pt/D')
-       #outTree.Branch('a1_pz',self.a1_pz,'a1_pz/D')
-       #outTree.Branch('a2_pz',self.a2_pz,'a2_pz/D')
-       #outTree.Branch('tp_mass',self.tp_mass,'tp_mass/D')
-       #outTree.Branch('pt_max_4gamma',self.pt_max_4gamma,'pt_max_4gamma/D')
-       #outTree.Branch('gen123_pt',self.gen123_pt,'gen123_pt/D')
-       #outTree.Branch('gen124_pt',self.gen124_pt,'gen124_pt/D')
-       #outTree.Branch('gen134_pt',self.gen134_pt,'gen134_pt/D')
-       #outTree.Branch('gen234_pt',self.gen234_pt,'gen234_pt/D')
+       outTree_case1.Branch('Pho1out',self.Pho1out,'Pho1out/D')
+       outTree_case1.Branch('Pho2out',self.Pho2out,'Pho2out/D')
+       outTree_case1.Branch('Pho3out',self.Pho3out,'Pho3out/D')
+       outTree_case1.Branch('Pho4out',self.Pho4out,'Pho4out/D')
 
        return outTree_case1
 
-
-
-
-   def MakeGenTree_case2(self):
-       outTree_case2 = TTree("H4GGen_case2", "1 fat + 2 resolved")
+   #
+   #
+   #
+   def MakeGenTree_2res1fat(self):
+       outTree_case2 = TTree("H4GGen_2res1fat", "1 fat + 2 resolved")
        SetOwnership(outTree_case2,0)
-   
+
        outTree_case2.Branch('isopho1_pt_case2', self.isopho1_pt_case2, 'isopho1_pt_case2/D')
        outTree_case2.Branch('isopho2_pt_case2', self.isopho2_pt_case2, 'isopho2_pt_case2/D')
        outTree_case2.Branch('isopho1_eta_case2', self.isopho1_eta_case2, 'isopho1_eta_case2/D')
        outTree_case2.Branch('isopho2_eta_case2', self.isopho2_eta_case2, 'isopho2_eta_case2/D')
        outTree_case2.Branch('fatpho_pt_case2', self.fatpho_pt_case2, 'fatpho_pt_case2/D')
        outTree_case2.Branch('fatpho_eta_case2', self.fatpho_eta_case2, 'fatpho_eta_case2/D')
-       outTree_case2.Branch('fatpho_dr_case3',self.fatpho_dr_case3,'fatpho_dr_case3/D')
+       outTree_case2.Branch('fatpho_isopho1_dr_case2',self.fatpho_isopho1_dr_case2,'fatpho_isopho1_dr_case2/D')
+       outTree_case2.Branch('fatpho_isopho2_dr_case2',self.fatpho_isopho2_dr_case2,'fatpho_isopho2_dr_case2/D')
+       outTree_case2.Branch('isopho1_isopho2_dr_case2',self.isopho1_isopho2_dr_case2,'isopho1_isopho2_dr_case2/D')
+       outTree_case2.Branch('fatpho_mass_case2', self.fatpho_mass_case2, 'fatpho_mass_case2/D')
+       outTree_case2.Branch('isopho12_mass_case2', self.isopho12_mass_case2, 'isopho12_mass_case2/D')
        outTree_case2.Branch('tp_mass_case2', self.tp_mass_case2, 'tp_mass_case2/D')
-   
+       outTree_case2.Branch('Fatphoout',self.Fatphoout,'Fatphoout/D')
+       outTree_case2.Branch('Isopho1out',self.Isopho1out,'Isopho1out/D')
+       outTree_case2.Branch('Isopho2out',self.Isopho2out,'Isopho2out/D')
        return outTree_case2
-   
 
-   
-   def MakeGenTree_case3(self):
-       outTree_case3 = TTree("H4GGen_case3", "2 fat")
+
+   def MakeGenTree_2fat(self):
+       outTree_case3 = TTree("H4GGen_2fat", "2 fat")
        SetOwnership(outTree_case3,0)
-       
+
        outTree_case3.Branch('fatpho1_pt_case3', self.fatpho1_pt_case3, 'fatpho1_pt_case3/D')
        outTree_case3.Branch('fatpho2_pt_case3', self.fatpho2_pt_case3, 'fatpho2_pt_case3/D')
        outTree_case3.Branch('fatpho1_eta_case3', self.fatpho1_eta_case3, 'fatpho1_eta_case3/D')
        outTree_case3.Branch('fatpho2_eta_case3', self.fatpho2_eta_case3, 'fatpho2_eta_case3/D')
-       outTree_case3.Branch('fatpho1_dr_case3',self.fatpho1_dr_case3,'fatpho1_dr_case3/D')
-       outTree_case3.Branch('fatpho2_dr_case3',self.fatpho2_dr_case3,'fatpho2_dr_case3/D')
+       outTree_case3.Branch('fatpho1_fatpho2_dr_case3',self.fatpho1_fatpho2_dr_case3,'fatpho1_fatpho2_dr_case3/D')
+       outTree_case3.Branch('fatpho1_mass_case3', self.fatpho1_mass_case3, 'fatpho1_mass_case3/D')
+       outTree_case3.Branch('fatpho2_mass_case3', self.fatpho2_mass_case3, 'fatpho2_mass_case3/D')
        outTree_case3.Branch('tp_mass_case3', self.tp_mass_case3, 'tp_mass_case3/D')
-   
-       return outTree_case3
+       outTree_case3.Branch('Fatpho1out',self.Fatpho1out,'Fatpho1out/D')
+       outTree_case3.Branch('Fatpho2out',self.Fatpho2out,'Fatpho2out/D')
 
+       return outTree_case3
