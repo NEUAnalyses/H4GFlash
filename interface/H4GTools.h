6e56db6f0709d9ccd06d57869b561520fa1c697e
wamorkart <wamorkar.t@husky.neu.edu>
2018-07-10 17:21:33 +0200

    First attempt to add Low mass preselection

--- interface/H4GTools.h
+++ interface/H4GTools.h
@@ -1,3 +1,17 @@
+
+//FLASHgg files
+// #include "flashgg/DataFormats/interface/DiPhotonCandidate.h"
+// #include "flashgg/DataFormats/interface/SinglePhotonView.h"
+// #include "flashgg/DataFormats/interface/Photon.h"
+// #include "flashgg/DataFormats/interface/Jet.h"
+// #include "DataFormats/VertexReco/interface/Vertex.h"
+// #include "DataFormats/Math/interface/LorentzVector.h"
+// #include "FWCore/Common/interface/TriggerNames.h"
+// #include "DataFormats/Common/interface/TriggerResults.h"
+// #include "flashgg/DataFormats/interface/Electron.h"
+// #include "flashgg/DataFormats/interface/Muon.h"
+
+
 //my includes
 #include <memory>
 #include <vector>
@@ -12,6 +26,12 @@
 #include "DataFormats/Math/interface/LorentzVector.h"
 #include "FWCore/Common/interface/TriggerNames.h"
 #include "DataFormats/Common/interface/TriggerResults.h"
+
+#include "flashgg/DataFormats/interface/DiPhotonCandidate.h"
+#include "flashgg/DataFormats/interface/SinglePhotonView.h"
+#include "flashgg/DataFormats/interface/Photon.h"
+#include "flashgg/DataFormats/interface/Jet.h"
+// #include "flashgg/Taggers/interface/GlobalVariablesDumper.h"
 
 namespace H4GTools {
 
@@ -34,6 +54,11 @@ namespace H4GTools {
         int idp2;
 	float SumPt;
       };
+      // bool passLowMassHggPreselection(const flashgg::DiPhotonCandidate * dipho);
+      // std::vector<flashgg::DiPhotonCandidate> DiPhotonPreselection( vector<flashgg::DiPhotonCandidate> diphoCol );
+
+      
+
 
       std::map<std::string,int> TriggerSelection(std::vector<std::string> myTriggers, const edm::TriggerNames &names, edm::Handle<edm::TriggerResults> triggerBits)
       {
@@ -43,7 +68,7 @@ namespace H4GTools {
             int accepted = 0;
             for ( unsigned int i = 0; i < triggerBits->size(); i++)
             {
-//            if(DEBUG) std::cout << "[bbggTools::TriggerSelection] Trigger name: " << names.triggerName(i) << " \t Decision: " << triggerBits->accept(i) << std::endl;
+            //if(DEBUG) std::cout << "[H4GTools::TriggerSelection] Trigger name: " << names.triggerName(i) << " \t Decision: " << triggerBits->accept(i) << std::endl;
                if((names.triggerName(i)).find(myTriggers[j]) != std::string::npos )
                {
                   if(triggerBits->accept(i) == 1){
@@ -56,5 +81,6 @@ namespace H4GTools {
          }
           return triggerResults;
       };
+
 
 }
