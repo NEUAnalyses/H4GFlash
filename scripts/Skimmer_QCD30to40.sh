c3116f582b129af104fc4f18cc5c3444ecb083a4
wamorkart <wamorkar.t@husky.neu.edu>
2018-08-31 17:09:50 +0200

    uppdate path

--- scripts/Skimmer_QCD30to40.sh
+++ scripts/Skimmer_QCD30to40.sh
@@ -1,5 +1,5 @@
 for i in {0..800};
 do
 echo $i
-  python H4GTreeSkimmer.py -i /eos/user/t/twamorka/2016_Background/output_QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCUETP8M1_13TeV_Pythia8_${i}.root  -o /eos/user/t/twamorka/SkimmedTrees_2016/QCD30to40_${i}.root
+  python ../TreeSkimmerPy/H4GTreeSkimmer.py -i /eos/user/t/twamorka/2016_Background/output_QCD_Pt-30to40_DoubleEMEnriched_MGG-80toInf_TuneCUETP8M1_13TeV_Pythia8_${i}.root  -o /eos/user/t/twamorka/SkimmedTrees_2016/QCD30to40_${i}.root
 done
--- scripts/Skimmer_Signal.sh
+++ scripts/Skimmer_Signal.sh
@@ -3,5 +3,5 @@
 for i in 0p1 1 5 10 15 20 25 30 35 40 45 50 55 60;
 do
 	echo $i
-       python H4GTreeSkimmer.py -i /eos/user/t/twamorka/2016_Signal/signal_m_${i}.root  -o /eos/cms/store/user/twamorka/FlatTrees_2016/Signal/signal_m_${i}.root
+       python ../TreeSkimmerPy/H4GTreeSkimmer.py -i /eos/user/t/twamorka/2016_Signal/signal_m_${i}.root  -o /eos/cms/store/user/twamorka/FlatTrees_2016/Signal/signal_m_${i}.root
 done
